ac2c915dcd08a25e69331c5c24b46f5d8262fa81
Andrew Smith <asmith@asmith2019.local>
2020-02-26 16:39:47 -0400

    Added ability to change the src

--- src/index.js
+++ src/index.js
@@ -6,15 +6,32 @@ export class HLSPlayer extends HTMLVideoElement {
   }
   constructor() {
     super();
-    this.hls = new Hls();
-    this.hls.attachMedia(this);
   }
+
+  /**
+   * Is called whenever a observed attribute changes
+   *
+   * @param {string} atb the attribute that changed
+   * @param {string} current the current attribute value
+   * @param {string} newValue the new attribute value
+   * @memberof HLSPlayer
+   */
   attributeChangedCallback(atb, current, newValue) {
-    if (current === newValue) {
+    if (
+      !newValue || //if value is not empty
+      current === newValue || //ignore if the value is the same
+      -1 < newValue.indexOf('blob:http') // ignore hls.js changing the src
+    ) {
       return;
     }
+
     switch (atb) {
       case 'src':
+        if (this.hls) {
+          this.hls.destroy();
+        }
+        this.hls = new Hls();
+        this.hls.attachMedia(this);
         this.hls.loadSource(newValue);
         break;
     }
